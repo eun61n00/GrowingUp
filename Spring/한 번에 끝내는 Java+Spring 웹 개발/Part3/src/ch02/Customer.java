4fe9d9f5f74bc604dc0e4cfe7247afdb5f8080ff
eunbin <eun61n@gmail.com>
2023-01-08 10:00:01 +0900

    java oop

---
+++ Spring/한 번에 끝내는 Java+Spring 웹 개발/Part3/src/ch02/Customer.java
@@ -0,0 +1,15 @@
+package ch02;
+
+public class Customer {
+
+    private int customerId;
+    private String customerName;
+    private String custormerGrade;
+    private int bonusPoint;
+    private double bonusRatio;
+
+    public Customer() {
+        this.custormerGrade = "SILVER";
+        this.bonusRatio = 0.01;
+    }
+}
